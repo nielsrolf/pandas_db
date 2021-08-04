696ec0738f9d2606708699bc0c4c5a2ba1a0cb5a
Niels Warncke <nielswarncke@gmail.com>
2021-08-04 19:13:10 +0200

    ec2 deployment wip

---
+++ ec2/restart.sh
@@ -0,0 +1,7 @@
+echo "Restarting..."
+wget https://pandasdb-ddsp-demo.s3.eu-central-1.amazonaws.com/.local_db.csv -O $PANDAS_DB_PATH/.local_db.csv
+pip install -e .
+python ec2/migrate_pandas_db.py
+cp pandas_db/views.json $PANDAS_DB_PATH/.pandas_db_views.json
+sudo kill -9 $(sudo lsof -t -i:8050)
+pandasdb metrics2
--- ec2/update_and_restart.sh
+++ ec2/update_and_restart.sh
@@ -3,10 +3,5 @@ git log > oldlog
 git pull
 git log > newlog
 if [ "$(diff oldlog newlog)" != "" ]; then
-    wget https://pandasdb-ddsp-demo.s3.eu-central-1.amazonaws.com/.local_db.csv -O $PANDAS_DB_PATH/.local_db.csv
-    pip install -e .
-    python ec2/migrate_pandas_db.py
-    cp pandas_db/views.json $PANDAS_DB_PATH/.pandas_db_views.json
-    sudo kill -9 $(sudo lsof -t -i:8050)
-    pandasdb metrics2
+    source ec2/restart.sh
 fi
