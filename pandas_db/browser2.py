e75b494fbdddbb4b60cd0162b362fadb12534de9
Niels Warncke <nielswarncke@gmail.com>
2021-08-05 23:09:53 +0200

    better design

--- pandas_db/browser2.py
+++ pandas_db/browser2.py
@@ -33,7 +33,7 @@ def main(view_name):
         views = json.load(json_file)
     view = views[view_name]
     app = init_app(keys=view['keys'], columns=view.get('columns'), file_id=view.get('file_id'))
-    app.run_server(host='0.0.0.0', port=8050)
+    app.run_server(host='0.0.0.0', port=8050, debug=("nielswarncke" in os.getcwd()))
 
 
 def jupyter(keys, columns, file_id, **server_args):
@@ -48,6 +48,27 @@ def jupyter(keys, columns, file_id, **server_args):
     """
     app = init_app(keys=keys, columns=columns, file_id=file_id, jupyter=True)
     app.run_server(**server_args)
+
+
+class State():
+    """Class that caches the state of pandas_db and imlements transaction search"""
+    def __init__(self, keys, columns, file_id):
+        self.keys = keys
+        self.columns = [c for c in columns if not c in self.keys]
+        self.file_id = file_id
+        self._transactions = None
+        self.file_info = None
+        self.fetch()
+
+    def fetch(self):
+        self._transactions = pandas_db.get_df()
+        self.file_info = pandas_db.latest(keys=["file"], df=self._transactions)
+        self.search = self.file_info.fillna("").reset_index()
+        self.search['search_index'] = self.search.apply(concat_as_str, axis=1)
+
+
+def concat_as_str(values):
+    return "".join([str(i) for i in values])
 
 
 def init_app(keys, columns, file_id, jupyter=False):
@@ -108,15 +129,22 @@ def init_app(keys, columns, file_id, jupyter=False):
         filtered_df, groupby_keys = filter_df(*dropdown_values)
         return get_metric_plot(filtered_df, state.columns, groupby_keys)
     
-    search = dcc.Input('global-search', type='text', style={"width": "100%", "height": "30px", "z-index": "10", "border": "2px solid #2cb2cb", "position": "fixed", "bottom": "1px"}, placeholder="Keyword search: enter any number of words you'd like to search")
+    search = dcc.Input('global-search', type='text', style={
+        "width": "100%", "height": "30px", "z-index": "10",
+        "border": "2px solid #2cb2cb", "position": "fixed", "bottom": "1px"},
+        placeholder="Keyword search: enter any number of words you'd like to search")
+    
+    cheap_pagination_1 = dcc.Input('file_id_first', type='number', style={"display": "inline"}, placeholder="0")
+    cheap_pagination_2 = dcc.Input('file_id_last', type='number', style={"display": "inline"}, placeholder="5")
+    pagination = html.Div([cheap_pagination_1, " - ", cheap_pagination_2], style={"margin": "auto", "width": "400px"})
 
     detail_view = html.Div([
         dropdowns_files,
+        pagination,
         search,
         dcc.Loading(html.Div(id='medias'))],
         style={
                 "width": "100%",
-                "background-color": "#2cb2cb",
                 "bottom": "30px"})
     @functools.lru_cache(maxsize=20)
     def global_search(*dropdown_values, search_str=""):
@@ -143,19 +171,20 @@ def init_app(keys, columns, file_id, jupyter=False):
 
     @app.callback(
         Output('medias', 'children'),
-        [Input('global-search', 'value')] + \
+        [Input('global-search', 'value'), Input('file_id_first', 'value'), Input('file_id_last', 'value')] + \
         [Input('dropdown-{}'.format(key), 'value') for key in dropdown_fields])
-    def update_medias_clb(search_str, *dropdown_values):
+    def update_medias_clb(search_str, file_id_first, file_id_last, *dropdown_values):
         dropdown_values = mask_dropdown_values(dropdown_values)
         df_files , groupby_keys = global_search(*dropdown_values, search_str=search_str)
         if len(df_files) == 0:
-            return
+            return None
         keys = [key for key in state.file_id if key not in groupby_keys] + groupby_keys
 
         df_files = df_files.fillna("")
         df_files = pandas_db.latest(keys=keys, df=df_files).reset_index()
         df_files = df_files.loc[(df_files.file!="") & (df_files.file != "?")]
-        return update_medias(state, df_files)
+        rel_paths = df_files["file"].values[(file_id_first or 0):(file_id_last or 5)]
+        return update_medias(state, rel_paths)
 
     app.layout = dbc.Container([
         dropdowns_top,
@@ -204,40 +233,19 @@ def get_metric_plot(df, metrics, groupby_keys):
     return metric_plots
 
 
-class State():
-    """Class that caches the state of pandas_db and imlements transaction search"""
-    def __init__(self, keys, columns, file_id):
-        self.keys = keys
-        self.columns = [c for c in columns if not c in self.keys]
-        self.file_id = file_id
-        self._transactions = None
-        self.file_info = None
-        self.fetch()
-
-    def fetch(self):
-        self._transactions = pandas_db.get_df()
-        self.file_info = pandas_db.latest(keys=["file"], df=self._transactions)
-        self.search = self.file_info.fillna("").reset_index()
-        self.search['search_index'] = self.search.apply(concat_as_str, axis=1)
-
-
-def concat_as_str(values):
-    return "".join([str(i) for i in values])
-
-
-def update_medias(state, df_files):
+def update_medias(state, rel_paths):
     medias = []
-    for rel_path in df_files["file"].values[:5]:
+    for rel_path in rel_paths:
         try:
             file_info = state.file_info.loc[rel_path]
-            medias += [show_media(state, rel_path, file_info)]
+            medias += [show_media(rel_path, file_info)]
         except (KeyError, FileNotFoundError, IndexError) as e:
             print(e)
             pass
     return medias
 
 
-def show_media(state, media_file, file_info):
+def show_media(media_file, file_info):
     if os.environ.get("PANDAS_DB_S3_PREFIX") is not None:
         if media_file.endswith(".png"):
             media = html.Img(src=f"{os.environ.get('PANDAS_DB_S3_PREFIX')}.pandas_db_files/{media_file}", style={"height": "300px", "width": "auto"})
@@ -259,7 +267,13 @@ def show_media(state, media_file, file_info):
     info = dash_table.DataTable(
         columns = [{"name": "", "id": "key"}, {"name": "", "id": "value"}],
         data=file_info.to_dict('records'),
-        style_cell={'padding': '5px', 'width': "100px"},
+        style_cell={'padding': '5px', 'width': "100px", "border": "0px"},
+        style_cell_conditional=[
+            {
+                'if': {'column_id': 'value'},
+                'textAlign': 'left'
+            }
+        ],
         style_header = {'display': 'none'}
     )
     return html.Div([html.Div(info, style={"flex": "50%"}),
@@ -271,7 +285,8 @@ def show_media(state, media_file, file_info):
                             "padding": "5px"})],
                         style={
                             "display": "flex", 
-                            "background-color": "rgba(0.9, 0.9, 0.9, 0.5)", 
+                            "border": "3px solid #2cb2cb", 
+                            "border-radius": "3px",
                             "margin-bottom": "5px", "margin-top": "5px"})
 
 
